196c7e18ec1b8f01c514cb0bcce41b077c310ad0
seyun4047 <love4047@naver.com>
2024-03-18 11:37:21 +0900

    [UPDATE] songRecByAi modules updated

---
+++ QRGen/QRGenerator.py
@@ -0,0 +1,78 @@
+import qrcode
+import cv2
+import numpy as np
+class QRGen:
+    def __init__(self, path, data):
+        self.img_path = path
+        print("img_path Ok")
+        self.data = data
+        print("data OK")
+        self.oriImg = cv2.imread(self.img_path)
+        print("oriImg OK")
+        self.img = self.oriImg.copy()
+        print("copy OK")
+        self.h, self.w = self.img.shape[:2]
+        self.qrGen()
+
+    def qrGen(self):
+        self.qrImgGen()
+        print("QR gened")
+        self.putQRonImg()
+        print("QR putQronImg")
+        self.writeImg()
+        print("writed", self.new_path)
+        return cv2.imread(self.new_path)
+    def writeImg(self):
+        self.new_path=self.img_path.split(".")[0] + "_gen." + self.img_path.split(".")[1]
+        cv2.imwrite(self.new_path,self.img)
+    def qrImgGen(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(self.data)
+        qr.make(fit=True)
+
+        self.qrImg = qr.make_image(fill_color="black", back_color="white")
+
+        # Pillow 이미지로 변환
+        self.qrImg = self.qrImg.convert("RGBA")
+
+        mlen = (int(max(self.h, self.w) / 6))
+        print(mlen)
+        self.qrImg = self.qrImg.resize((mlen, mlen))
+        # 투명 배경 생성
+        datas = self.qrImg.getdata()
+        new_data = []
+        for item in datas:
+            # 흰색 픽셀을 투명하게 처리
+            if item[0]==0 and item[1]==0 and item[2]==0:
+                new_data.append(item)
+            else:
+                new_data.append((255, 255, 255, 0))
+
+        self.qrImg.putdata(new_data)
+
+    def putQRonImg(self):
+        self.qrImg = np.array(self.qrImg)
+
+        _, mask = cv2.threshold(self.qrImg[:, :, 3], 1, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
+
+        qrImg = cv2.cvtColor(self.qrImg, cv2.COLOR_BGRA2BGR)
+        qrh, qrw = qrImg.shape[:2]
+        roi = self.img[self.h - qrh - 10:self.h - 10, self.w - qrw - 10:self.w - 10]
+
+        masked_qrImg = cv2.bitwise_and(qrImg, qrImg, mask=mask)
+        masked_img = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+        added = masked_qrImg + masked_img
+        self.img[self.h - qrh - 10:self.h - 10, self.w - qrw - 10:self.w - 10] = added
+
+        # 합성된 이미지 저장
+        # cv2.imwrite('img_gen.jpeg', self.img)
+
+#
+# a = QRGen("test.jpg","https://mutzin.site")
---
+++ recSongGen/songRecByAi_gemini.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+import requests
+import sys
+import google.generativeai as genai
+from googleapiclient.discovery import build
+
+sys.path.append("../")
+from ..QRGen import QRGenerator
+
+
+class SongRecByAi:
+    def __init__(self, path):
+        self.path = path
+        self.gen()
+
+    def gen(self):
+        songTitle = self.songGen()
+        songUrl = self.get_video_links(songTitle)
+        self.check_link_validity(songUrl)
+        QRGenerator.QRGen(self.path, songUrl)
+
+    def check_link_validity(self, url):
+        try:
+            response = requests.head(url, allow_redirects=True)
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+        except requests.ConnectionError:
+            print("error")
+            return False
+
+    def songGen(self):
+        GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+        image = cv2.imread(self.path)
+
+        # print(image)
+        def getResponseGemini():
+            model = genai.GenerativeModel('gemini-pro')
+            # response = model.generate_content(f"이미지의 텍스트를 주면 이에 어울리는 분위기를 파악하고 노래를 추천해줘. 노래는 k-pop 45%, j-pop 35%, global-music 20% 우선순위를 가져. 오래되지않은 노래면 좋겠어. 대답은 무조건 노래제목-가수이름 혹은 가수이름-노래제목 으로 알려줘. 예를들어 이렇게 대답해. 라일락-아이유. 다음은 이미지야. {image}")
+            response = model.generate_content(f"If I give you the text of an image, it will identify the mood that matches it and recommend a song. Songs have 45% priority on k-pop, 35% on j-pop, and 20% on global-music. I hope it's a song that isn't old. Please tell me the answer as song title-singer name or singer name-song title. For example, answer like this: Lilac - IU. Next is the image. {image}")
+            return response
+
+        response = getResponseGemini()
+        response = response.text
+        print(response)
+
+        return response
+    def get_video_links(self, query):
+        DEVELOPER_KEY = os.getenv('YOUTUBE_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=DEVELOPER_KEY)
+
+        search_response = youtube.search().list(
+          q=query,
+          order="relevance",
+          part="snippet",
+          maxResults=1,
+          type="video"  # 비디오만 검색 결과로 받기
+        ).execute()
+
+        video_link = ''
+        for search_result in search_response.get('items', []):
+          if search_result['id']['kind'] == 'youtube#video':
+            video_id = search_result['id']['videoId']
+            video_link = f"https://www.youtube.com/watch?v={video_id}"
+
+        return video_link
+# SongRecByAi("test2.jpeg")
